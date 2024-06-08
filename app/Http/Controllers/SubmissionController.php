2a5fbae984c5e3084719ccd4e3a4a0896e438a42
naumanjunaid <nauman.junaid@gmail.com>
2024-06-08 15:59:30 +0500

    submission request validator added

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\SubmissionRequest;
 
 class SubmissionController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SubmissionRequest $request)
     {
         //
     }
