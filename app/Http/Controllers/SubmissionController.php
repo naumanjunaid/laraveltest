82218d004005051292daf2006c9e653c41c67709
naumanjunaid <nauman.junaid@gmail.com>
2024-06-08 15:55:00 +0500

    created submittion resource and route

---
+++ app/Http/Controllers/SubmissionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubmissionController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
